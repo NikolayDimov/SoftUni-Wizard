627ca3397996d3125b8dbb64fa14271304b11971
NikolayDimov <114500017+NikolayDimov@users.noreply.github.com>
2023-01-29 22:36:39 +0200

    Wizard refactoring

---
+++ src/engine.js
@@ -0,0 +1,51 @@
+let keys = {};
+let player = {
+    x: 150,
+    y: 100,
+    width: 0,
+    height: 0,
+    lastTimeFiredFireball: 0,
+};
+
+let gameInfo = {
+    isActiveGame: true,
+    score: 0,
+    lastCloudSpawn: 0,
+    lastBugSpawn: 0,
+};
+
+
+function isCollision(firstElement, secondElement) {
+    let firstRec = firstElement.getBoundingClientRect();
+    let secondRec = secondElement.getBoundingClientRect();
+
+    return !(firstRec.top > secondRec.bottom ||
+             firstRec.bottom < secondRec.top ||
+             firstRec.right < secondRec.left ||
+             firstRec.left > secondRec.right)
+}
+
+function gameOverFn() {
+    gameInfo.isActiveGame = false;
+    gameOver.classList.remove('hide');
+}
+
+function addFireBall(player) {
+    let fireBall = document.createElement('div');
+    fireBall.classList.add('fire-ball')
+    fireBall.style.top = (player.y + player.height / 3) + 'px';
+    fireBall.x = player.x + player.width;
+    fireBall.style.left = fireBall.x + 'px';
+    gameArea.appendChild(fireBall);
+}
+
+
+function onKeyDown(event) {
+    event.preventDefault();
+    keys[event.code] = true;
+    console.log(keys);
+}
+
+function onKeyUp(event) {
+    keys[event.code] = false;
+}
---
+++ src/tools.js
@@ -0,0 +1,10 @@
+let game = {
+    speed: 2,
+    frameLength: 30,
+    movingMultiplayer: 3,
+    fireBallMultiplayer: 4,
+    fireInterval: 1000,
+    cloudSpawnInterval: 3000,
+    buhSpawnInterval: 1000,
+    bugKillBonus: 2000,
+};
